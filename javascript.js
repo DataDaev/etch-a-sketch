653058baa3ed9f2635ce267b13b8fab14bcd6c6f
Davis Tran <datadaev@gmail.com>
2023-07-14 23:56:56 -0700

    Added hover color effect and prompt

--- javascript.js
+++ javascript.js
@@ -1,9 +1,19 @@
 function makeGrid() {
-    const container = document.getElementById('gridContainer');
-    const gridRows = 16;
-    const gridColumns = 16;
+    let userGridSize = prompt('What size grid would you like? Max size is 100', '');
+    let validPrompt = false;
 
-    for(i = 0; i < gridRows * gridColumns; i++) {
+    while (!validPrompt) {
+        if (userGridSize <= 100 && userGridSize !== null && !isNaN(userGridSize) && userGridSize.trim() !== '') {
+            validPrompt = true;
+        } else {
+            userGridSize = prompt('Invalid entry. Please enter a number 100 or below')
+        }
+    }
+
+    const container = document.getElementById('gridContainer');
+    container.style.gridTemplateColumns = `repeat(${userGridSize}, 11px)`;
+
+    for (i = 0; i < userGridSize * userGridSize; i++) {
         const gridSquare = document.createElement('div');
         gridSquare.setAttribute('class', 'gridSquare');
       
@@ -11,5 +21,24 @@ function makeGrid() {
     }
 
 }
-
 makeGrid();
+
+const squareHover = document.querySelectorAll('.gridSquare');
+
+squareHover.forEach(hover => {
+    hover.addEventListener('mouseenter', squareColorChange);
+})   
+
+function squareColorChange(hover) {
+    hover.target.classList.add('hovered');
+    squareHover.forEach(hover => {
+        if (hover.classList.contains('hovered')) {
+            hover.style.backgroundColor = "blue"
+        }
+    })   
+}
+
+
+
+
+
