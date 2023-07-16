fa1943ac099eaa015420a12ff8ebe75fa317c6e3
Davis Tran <datadaev@gmail.com>
2023-07-15 21:48:07 -0700

    Added button to create grid

--- javascript.js
+++ javascript.js
@@ -1,16 +1,18 @@
+const container = document.getElementById('gridContainer');
+
 function makeGrid() {
-    let userGridSize = prompt('What size grid would you like? Max size is 100', '');
+    container.innerHTML = '';
+    let userGridSize = prompt('What size grid would you like? Max size is 100.', '');
     let validPrompt = false;
 
     while (!validPrompt) {
-        if (userGridSize <= 100 && userGridSize !== null && !isNaN(userGridSize) && userGridSize.trim() !== '') {
+        if (userGridSize <= 100 && userGridSize > 0 && userGridSize !== null && !isNaN(userGridSize) && userGridSize.trim() !== '') {
             validPrompt = true;
         } else {
-            userGridSize = prompt('Invalid entry. Please enter a number 100 or below')
+            userGridSize = prompt('Invalid entry. Please enter a number between 1 and 100.')
         }
     }
 
-    const container = document.getElementById('gridContainer');
     container.style.gridTemplateColumns = `repeat(${userGridSize}, 11px)`;
 
     for (i = 0; i < userGridSize * userGridSize; i++) {
@@ -20,23 +22,25 @@ function makeGrid() {
         container.appendChild(gridSquare);
     }
 
+    const squareHover = document.querySelectorAll('.gridSquare');
+    squareHover.forEach(hover => {hover.addEventListener('mouseenter', squareColorChange);})   
 }
-makeGrid();
-
-const squareHover = document.querySelectorAll('.gridSquare');
-
-squareHover.forEach(hover => {
-    hover.addEventListener('mouseenter', squareColorChange);
-})   
 
 function squareColorChange(hover) {
+    const squareHover = document.querySelectorAll('.gridSquare');
     hover.target.classList.add('hovered');
     squareHover.forEach(hover => {
         if (hover.classList.contains('hovered')) {
             hover.style.backgroundColor = "blue"
         }
-    })   
+    })  
 }
+
+makeGrid();
+
+const gridButton = document.getElementById('createGrid').addEventListener('click', () => {makeGrid()});
+
+
 
 
 
